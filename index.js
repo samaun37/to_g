f85e30767aaba8fa2b458b1239cf1010a906874c
samaun37 <76416089+samaun37@users.noreply.github.com>
2021-12-05 06:47:20 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,63 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors  from "cors"
+import bcrypt from 'bcrypt'
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+app.listen(3001, () => {
+    console.log("running on port 3001");
+  });
 
-import './index.css';
-import App from './App';
-
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+mongoose.connect(
+  "mongodb://localhost:27017/myapp1",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
 );
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    age: String,
+    department: String,
+    salary: String
+})
+const Employee = new mongoose.model("Employee",userSchema);
+
+app.post("/addnew", async (req, res) => {
+    console.log(req.body)
+
+  const { name, age, department, salary} = req.body;
+
+  Employee.findOne({name: name}, (err, emp) => {
+    if(emp){
+        res.send({message: "Employee already exists"})
+    } else {
+        const emp = new Employee({
+            name,
+            age,
+            department,
+            salary
+        })
+        emp.save(err => {
+            if(err) {
+                res.send({ message: "Some Error Occured." })
+            } else {
+                res.send( { message: "Employee Successfully Added." })
+            }
+        })
+    }
+})
+});
+
+app.get("/showrecords", async (req, res) => {
+    Employee.find({},(e,result) => {
+        if(e) res.send(e);
+        else res.send(result);
+    }).sort('-date').limit(2);
+});
+app.get("/", async (req, res) => {
+  res.send("Backend Api");
+});
